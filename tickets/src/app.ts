18d381a9c62ca12bbf70e6f0ea98b56e11a5f076
Quado2 <engchikwas404@gmail.com>
2024-01-16 20:53:16 +0100

    install needed depencies for tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,10 +4,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@qdtickets/common';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
+
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,10 +16,7 @@ app.use(
   })
 );
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
+
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
